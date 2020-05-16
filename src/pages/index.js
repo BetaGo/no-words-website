388d9bad91703905e302bcd0ac7116cac5ae4f38
BetaGo <gouwantong@live.com>
2020-05-16 11:39:58 +0800

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,13 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
+  <div style={{ display: "flex", width: "100vw", height: "100vh" }}>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div style={{ margin: "auto" }}>
+      <h1>No Words</h1>
+      <p>更懂你的单词学习工具</p>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  </div>
 )
 
 export default IndexPage
